b0a9d6ef24d60c0cb09e50784cb211ae161a3278
ylgr <quyen.luminous@gmail.com>
2021-02-08 16:39:19 +0700

    (+) function template

--- DecentralizeWalletManager.js
+++ DecentralizeWalletManager.js
@@ -1,4 +1,5 @@
-const { supportWallet} = require('./utils/contants')
+const { supportWallet} = require('./utils/contants');
+
 let isConnected = true;
 let currentAddress = '0x1eCE3c290B1B6b388bffe0002eAb1dF51356a9Df'
 let currentWalletType = supportWallet.metamask
@@ -19,11 +20,60 @@ function checkSupportedWalletsType() {
     return []
 }
 
+async function connectWallet(walletType, timeout) {
+
+}
+
+async function importPrivateKey(privateKey, password) {
+
+}
+
+async function importSeedPhrase(privateKey, password) {
+
+}
+
+async function importKeyStore(fileContent) {
+
+}
+
+async function createWallet(password) {
+
+}
+
+function exportPrivateKey(password) {
+
+}
+
+function calculateEstimatedGas(to, amount, tokenSymbol) {
+
+}
+
+async function send (password, to, amount, tokenSymbol, gasPrice, gasLimit, callback) {
+
+}
+
+function logout() {
+
+}
+
+async function getBalances() {
+
+}
+
 module.exports = {
     currentAddress,
     currentWalletType,
     supportedWalletsType,
     tokens,
     init,
-    checkSupportedWalletsType
+    checkSupportedWalletsType,
+    connectWallet,
+    importPrivateKey,
+    importSeedPhrase,
+    importKeyStore,
+    createWallet,
+    exportPrivateKey,
+    calculateEstimatedGas,
+    send,
+    logout
 }
