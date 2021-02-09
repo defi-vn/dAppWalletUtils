4d6079ac06855659df79db885b019a287f65a99f
ylgr <quyen.luminous@gmail.com>
2021-02-09 12:34:12 +0700

    (+) DecentralizeWalletManager.js

--- DecentralizeWalletManager.js
+++ DecentralizeWalletManager.js
@@ -1,28 +1,26 @@
 const { supportWallet} = require('./utils/contants');
 const Web3 = require('web3');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
+const {generateMnemonic} = require('bip39');
+const BigNumber = require('bignumber.js')
+const erc20Abi = require('./contracts/erc20.abi.json')
 
+const network = 'TESTNET'
+// const network = 'MAINNET'
 
+const supportSymbol = network === 'TESTNET'? require('./tokens/supportSymbolTest') : require('./tokens/supportSymbol')
+
+let currentAddress = '0x1eCE3c290B1B6b388bffe0002eAb1dF51356a9Df';
+let supportedWalletsType = Object.values(supportWallet);
+
+let keystore = null;
 let isConnected = true;
-let currentAddress = '0x1eCE3c290B1B6b388bffe0002eAb1dF51356a9Df'
-let currentWalletType = supportWallet.metamask
-let supportedWalletsType = Object.values(supportWallet)
-let keystore = null
-
-let tokens = [
-    {
-        symbol: 'BNB',
-        address: '',
-        amount: 0
-    }
-]
+let currentWalletType = null;
+let tokens = []
 
 async function init() {
 
 }
-
-const network = 'TESTNET'
-// const network = 'MAINNET'
 
 function checkSupportedWalletsType() {
     let result = [supportWallet.dfyWallet]
@@ -48,7 +46,9 @@ async function connectWallet(walletType, timeout) {
         web3 = new Web3(window.BinanceChain)
         currentWalletType = supportWallet.binanceChain
     }
-    
+    const accounts = await web3.eth.getAccounts()
+    currentAddress = accounts[0]
+    await getBalances()
 }
 
 function generateKeystore(provider, password) {
@@ -58,9 +58,10 @@ function generateKeystore(provider, password) {
     const wallet = provider.wallets[currentAddress]
 
     keystore = web3.eth.accounts.encrypt(wallet.getPrivateKeyString(), password)
+    getBalances()
 }
 
-async function importPrivateKey(privateKey, password) {
+function importPrivateKey(privateKey, password) {
     const provider = new HDWalletProvider({
         privateKeys: [
             privateKey
@@ -72,7 +73,7 @@ async function importPrivateKey(privateKey, password) {
 
 }
 
-async function importSeedPhrase(seedPhrase, password) {
+function importSeedPhrase(seedPhrase, password) {
     const provider = new HDWalletProvider({
         mnemonic: {
             phrase: seedPhrase
@@ -83,35 +84,119 @@ async function importSeedPhrase(seedPhrase, password) {
     generateKeystore(provider, password)
 }
 
-async function importKeyStore(fileContent) {
+function importKeyStore(fileContent) {
     currentAddress = '0x' + fileContent.address
     keystore = fileContent
     web3 = new Web3("https://data-seed-prebsc-1-s1.binance.org:8545")
-
+    getBalances()
 }
 
-async function createWallet(password) {
+function createWallet(password) {
+    const mnemonic = generateMnemonic()
+    const provider = new HDWalletProvider({
+        mnemonic: {
+            phrase: mnemonic
+        },
+        providerOrUrl: "https://data-seed-prebsc-1-s1.binance.org:8545"
+    });
+    const address = Object.keys(provider.wallets)[0]
+    const wallet = provider.wallets[address]
 
+    const web3 = new Web3(provider)
+
+    const keystore = web3.eth.accounts.encrypt(wallet.getPrivateKeyString(), password)
+
+    return {
+        mnemonic: mnemonic,
+        privateKey: wallet.getPrivateKeyString(),
+        address: address,
+        keystore: keystore
+    }
 }
 
 function exportPrivateKey(password) {
-
+    const account = web3.eth.accounts.decrypt(keystore, password)
+    return account.getPrivateKeyString()
 }
 
-function calculateEstimatedGas(to, amount, tokenSymbol) {
-
+async function calculateEstimatedGas(to, amount, tokenSymbol) {
+    return web3.eth.getGasPrice()
 }
 
 async function send (password, to, amount, tokenSymbol, gasPrice, gasLimit, callback) {
-
+    const account = web3.eth.accounts.decrypt(keystore, password)
+    if(tokenSymbol === 'BNB') {
+        const tx = {
+            from: currentAddress,
+            to: to,
+            value: new BigNumber(
+                amount
+            ).multipliedBy(10 ** 18).toString(),
+            gas: gasLimit,
+            gasPrice: gasPrice
+        }
+        const signed = await account.signTransaction(tx)
+        console.log('signed: ', signed)
+        const receipt = await web3.eth.sendSignedTransaction(signed.rawTransaction)
+        getBalances()
+        return receipt
+    } else {
+        const tokenContract = new web3.eth.Contract(
+            erc20Abi,
+            supportSymbol[tokenSymbol]
+        )
+        const txData = tokenContract.methods.transfer(
+            to,
+            new BigNumber(
+                amount
+            ).multipliedBy(10 ** 18).integerValue()
+        ).encodeABI();
+        const tx = {
+            from: currentAddress,
+            to: supportSymbol[tokenSymbol],
+            value: 0,
+            gas: gasLimit,
+            gasPrice: gasPrice,
+            data: txData
+        };
+        const signed = await account.signTransaction(tx)
+        console.log('signed: ', signed)
+        const receipt = await web3.eth.sendSignedTransaction(signed.rawTransaction)
+        getBalances()
+        return receipt
+    }
 }
 
 function logout() {
-
+    keystore = null;
+    isConnected = true;
+    currentWalletType = null;
+    tokens = []
 }
 
 async function getBalances() {
-
+    tokens = Object.keys(supportSymbol).map(async (symbol) => {
+        if(symbol === 'BNB') {
+            const userBalance = web3.eth.getBalance(currentAddress)
+            return {
+                symbol: symbol,
+                balance: userBalance
+            }
+        } else {
+            const address = supportSymbol[symbol]
+            const tokenContract = new web3.eth.Contract(
+                erc20Abi,
+                address
+            )
+            const userBalance = await tokenContract.methods
+                .balanceOf(currentAddress)
+                .call()
+            return {
+                symbol: symbol,
+                balance: userBalance
+            }
+        }
+    })
 }
 
 module.exports = {
